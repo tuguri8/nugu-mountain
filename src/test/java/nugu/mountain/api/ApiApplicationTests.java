2ded8f85182adce64249eac3f503a32e9dc9f7a7
한승우 <tuguri8@gmail.com>
2019-08-12 01:42:14 +0900

    test

--- src/test/java/nugu/mountain/api/ApiApplicationTests.java
+++ src/test/java/nugu/mountain/api/ApiApplicationTests.java
@@ -2,15 +2,19 @@ package nugu.mountain.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiApplicationTests {
+    private static final Logger log = LoggerFactory.getLogger(ApiApplicationTests.class);
 
     @Test
-    public void contextLoads() {
+    public void 테스트() {
+        log.info("test");
     }
 
 }
