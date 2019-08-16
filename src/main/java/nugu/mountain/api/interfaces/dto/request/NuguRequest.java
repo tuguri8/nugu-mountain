b5e29ff967ecee72951755cdba912472322353e5
한승우 <tuguri8@gmail.com>
2019-08-16 17:35:13 +0900

    #2 NuguRequest JsonIgnoreProperties 추가

--- src/main/java/nugu/mountain/api/interfaces/dto/request/NuguRequest.java
+++ src/main/java/nugu/mountain/api/interfaces/dto/request/NuguRequest.java
@@ -1,9 +1,11 @@
 package nugu.mountain.api.interfaces.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NuguRequest {
     private String version;
     private Action action;
