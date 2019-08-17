9ea3974c19dfa3f420e9a5345be59e99de9b1beb
한승우 <tuguri8@gmail.com>
2019-08-17 22:44:01 +0900

    #2 DAY Entity type 파싱 테스트

--- src/main/java/nugu/mountain/api/application/service/NuguServicempl.java
+++ src/main/java/nugu/mountain/api/application/service/NuguServicempl.java
@@ -166,7 +166,7 @@ public class NuguServicempl implements NuguService {
     @Override
     public NuguResponse getMntClimbingCondition(JsonNode parametersFromNuguRequest) {
         Mountain mountain = mountainService.getMountainFromName(parametersFromNuguRequest.get("mountain").get("value").asText());
-        log.info(parametersFromNuguRequest.get("BID_DT_DAY").asText());
+        log.info("로그 " + parametersFromNuguRequest.asText());
         String dayParameter = parametersFromNuguRequest.get("BID_DT_DAY").get("type").asText();
         Air air = weatherService.getAirFromAreaCode(mountain.getAreaCode());
         MountainFire mountainFire = mountainService.getMountainFireFromAreaCode(mountain.getAreaCode());
