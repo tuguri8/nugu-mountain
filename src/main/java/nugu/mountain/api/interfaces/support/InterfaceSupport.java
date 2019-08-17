90569de2d94275c675828adcb1dfd27069c7d8a3
한승우 <tuguri8@gmail.com>
2019-08-18 03:12:56 +0900

    #2 DAY Entity 로깅

--- src/main/java/nugu/mountain/api/interfaces/support/InterfaceSupport.java
+++ src/main/java/nugu/mountain/api/interfaces/support/InterfaceSupport.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nugu.mountain.api.interfaces.dto.request.NuguRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class InterfaceSupport {
 
+    private static final Logger log = LoggerFactory.getLogger(InterfaceSupport.class);
+
     public static JsonNode getParametersFromNuguRequest (NuguRequest nuguRequest) throws IOException {
+        log.info(nuguRequest.toString());
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readTree(objectMapper.writeValueAsString(nuguRequest.getAction().getParameters()));
     }
